0423a104895f5b46c764cf0d88e38949be833fed
bowo-prasetyo <bpraz@yahoo.com>
2026-07-19 14:41:12 +0900

    Create Contact.js

---
+++ js/pages/Contact.js
@@ -0,0 +1,58 @@
+export default {
+
+template:`
+
+<section>
+
+<h1>
+Kontak
+</h1>
+
+
+<p>
+
+Silakan hubungi Jakarta SaaS
+untuk informasi produk dan integrasi.
+
+</p>
+
+
+<form>
+
+<label>
+Nama
+</label>
+
+<br>
+
+<input type="text">
+
+
+<br><br>
+
+
+<label>
+Email
+</label>
+
+<br>
+
+<input type="email">
+
+
+<br><br>
+
+
+<button>
+Kirim
+</button>
+
+
+</form>
+
+
+</section>
+
+`
+
+}
