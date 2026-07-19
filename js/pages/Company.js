da24e24fa33c08703d5a7edf5c0c09f30f0aba10
bowo-prasetyo <bpraz@yahoo.com>
2026-07-19 14:40:41 +0900

    Create Company.js

---
+++ js/pages/Company.js
@@ -0,0 +1,26 @@
+export default {
+
+template:`
+
+<section>
+
+<h1>
+Tentang Jakarta SaaS
+</h1>
+
+
+<p>
+
+Jakarta SaaS membangun micro SaaS
+yang membantu perusahaan Indonesia
+melakukan digitalisasi proses bisnis
+dengan solusi yang sederhana dan efisien.
+
+</p>
+
+
+</section>
+
+`
+
+}
