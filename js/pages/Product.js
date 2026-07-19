c84fa668ed79be4e50ad9b52cc9a417b5fc3b08f
bowo-prasetyo <bpraz@yahoo.com>
2026-07-19 14:23:46 +0900

    Create Product.js

---
+++ js/pages/Product.js
@@ -0,0 +1,72 @@
+export default {
+
+
+template:`
+
+<section>
+
+
+<h1>
+SiTrEks
+</h1>
+
+
+<h2>
+Sistem Struk Ekstraktor
+</h2>
+
+
+<p>
+
+SiTrEks adalah API SaaS yang
+mengubah gambar struk menjadi
+data JSON terstruktur.
+
+</p>
+
+
+
+<h3>
+Alur kerja
+</h3>
+
+
+<ol>
+
+<li>
+User upload gambar struk
+</li>
+
+<li>
+API memproses gambar
+</li>
+
+<li>
+Sistem mengembalikan JSON
+</li>
+
+</ol>
+
+
+
+<h3>
+Contoh output
+</h3>
+
+
+<pre>
+
+{
+ "merchant":"Toko ABC",
+ "date":"2026-07-19",
+ "total":125000
+}
+
+</pre>
+
+
+</section>
+
+`
+
+}
